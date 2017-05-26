a849f54d52273e03c36f15e4d67d10dd2fd908a9
後藤恭智 <yasutomo.goto@xenophy.com>
2017-05-26 16:27:39 +0900

    vol2追加

--- src/main.js
+++ src/main.js
@@ -12,46 +12,29 @@ Vue.config.productionTip = false
 //   components: { App }
 // })
 
-var StarIcon = Vue.extend({
-  props: {
-    size: {
-      type: Number,
-      default: 20
-    }
-  },
-  template:
-    '<svg :width="size" :height="size" viewBox="0 0 16 16">' +
-    '<path fill="#FBC02D" d="M16 6.2l-5.3-1.12-2.7-4.69v13.01l4.94 2.21-.57-5.39 3.63-4.02zm0 0"/>' +
-    '<path fill="#FDD835" d="M5.3 5.09l-5.3 1.11 3.63 4.02-.57 5.39 4.94-2.21v-13.01l-2.7 4.7zm0 0"/>' +
-    '</svg>'
-});
-
-var AbsoluteNumber = Vue.extend({
-  props: {
-    value: {
-      type: Number,
-      default: 0
-    }
-  },
-  template:
-    '<div>{{ Math.abs( value ) }}</div>'
-});
-
 var componentRoot = new Vue({
-
-  template: '<div>' +
-    '<star-icon></star-icon>' +
-    'hello!' +
-    '<star-icon :size="40"></star-icon>' +
-    '<star-icon :size="60"></star-icon>' +
-    '<absolute-number :value="100"></absolute-number>' +
-    '<absolute-number :value="-50"></absolute-number>' +
+  data: function () {
+    return {
+      answer: null
+    }
+  },
+  template:
+    '<div>' +
+    '<p>あなたが選んだ選択肢は {{ answer }}</p>' +
+    '<button type="button" @click="setAnswer( true )">O</button>' +
+    '<button type="button" @click="setAnswer( false )">X</button>' +
+    '<div v-if="answer === true">' +
+    '正解です' +
+    '</div>' +
+    '<div v-if="answer === false">' +
+    '間違いです' +
+    '</div>' +
     '</div>',
-  components: {
-    AbsoluteNumber: AbsoluteNumber,
-    StarIcon: StarIcon
+  methods: {
+    setAnswer: function ( value ) {
+      this.$data.answer = value;
+    }
   }
-
 });
 
 componentRoot.$mount( '#MyAppRoot' );
